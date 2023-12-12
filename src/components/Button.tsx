2fd532c242cd520531057939b6a5db1f529318ca
Richard Carrigan <me@richardcarrigan.com>
2023-12-11 22:50:55 -0800

    Changes buttons to blue to match NKSC branding

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -7,7 +7,7 @@ export default function Button({
 }: PrismicNextLinkProps) {
   return (
     <PrismicNextLink
-      className={clsx("block w-fit bg-cyan-700 hover:bg-cyan-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider", className)}
+      className={clsx("block w-fit bg-blue-700 hover:bg-blue-800 transition-color duration-200 ease-in-out py-3 px-12 rounded-full font-display text-white font-bold text-base tracking-wider", className)}
       {...restProps}
     />
   )
